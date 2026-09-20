6814534a71f7e5151f4533e0c62becf44e972013
AnNguyen-Script <nguyenminhan952011@gmail.com>
2026-09-20 17:53:47 +0700

    Update Rivals_Bundled_Standalone.lua via Antigravity Sync

--- Rivals_Bundled_Standalone.lua
+++ Rivals_Bundled_Standalone.lua
@@ -256,7 +256,7 @@ Shared.Settings = {
     KeybindsOverlay = true,
     ThemeName = "Dark",
     AimHotkey = Enum.KeyCode.N,
-    AutoFireHotkey = Enum.KeyCode.M,
+    AutoFireHotkey = Enum.KeyCode.F2,
     NoRecoilHotkey = Enum.KeyCode.F1,
     AutoTeleportHotkey = Enum.KeyCode.E,
     SpeedTeleHotkey = Enum.KeyCode.T,
@@ -4785,7 +4785,7 @@ if hideHitboxToggleFrame then
 end
 
 local PanelAimbotSet = CreatePanel(TabAimbot, "Exploits", "", 0.5, 0, 0.5, 1)
-CreateToggle(PanelAimbotSet, "Kill Aura", Theme.DotRed, "AutoFire", function(v) Settings.AutoFire = v end)
+CreateToggleWithKeybind(PanelAimbotSet, "Kill Aura", Theme.DotRed, "AutoFire", "AutoFireHotkey", function(v) Settings.AutoFire = v end, function(v) Settings.AutoFireHotkey = v end)
 CreateToggle(PanelAimbotSet, "Wall Check ", Theme.DotRed, "AutoFireWallCheck", function(v) Settings.AutoFireWallCheck = v end)
 CreateToggle(PanelAimbotSet, 'Slient Aim <font color="#ff3333">[BETA]</font>', Theme.DotRed, "AutoFireHoldM2", function(v) Settings.AutoFireHoldM2 = v end)
 CreateToggleWithKeybind(PanelAimbotSet, 'NO RECOIL <font color="#ff3333">[BETA]</font>', Theme.DotRed, "NoRecoil", "NoRecoilHotkey", function(v) Settings.NoRecoil = v end, function(v) Settings.NoRecoilHotkey = v end)
@@ -5271,7 +5271,7 @@ UserInputService.InputBegan:Connect(function(input, gpe)
         Settings.AutoFire = not Settings.AutoFire
         if UI_Elements.AutoFire then UI_Elements.AutoFire.SetValue(Settings.AutoFire) end
         UpdateTabDots()
-        SendNotification("Hotkey", "Auto Fire: " .. (Settings.AutoFire and "BẬT" or "TẮT"))
+        SendNotification("Hotkey", "Kill Aura: " .. (Settings.AutoFire and "BẬT" or "TẮT"))
     elseif input.KeyCode == Settings.AutoTeleportHotkey and Settings.AutoTeleportHotkey ~= Enum.KeyCode.None then
         if not Settings.AutoTeleport and not CheckAndBypassCharacterAC("Auto Teleport") then
             return

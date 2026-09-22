999267611b86c8db1ea1723fe34509ee8a7b9190
AnNguyen-Script <nguyenminhan952011@gmail.com>
2026-09-22 13:08:16 +0700

    Update Shared.lua via Antigravity Sync

--- Shared.lua
+++ Shared.lua
@@ -206,7 +206,7 @@ end
 
 -- CẤU HÌNH GỐC (SETTINGS)
 Shared.Settings = {
-    AimEnabled = false, AimHoldMode = false, AimSafe = false, AimDist = 1000,
+    AimEnabled = false, AimHoldMode = false, AimSafe = false, AimSafeHeadshotRate = 65, AimDist = 1000,
     TargetPart = "Head", WallCheck = false, TeamCheck = true, SafeShieldCheck = false,
     FOV = 170, FOVVisible = false, AimSnapline = false,
     AimSmoothness = 0.75, AimJitter = 0, ReactionDelay = 0,

a231ca7c5f49746024b761b7db3b3125b5b10af6
AnNguyen-Script <nguyenminhan952011@gmail.com>
2026-09-22 13:16:17 +0700

    Update UI.lua via Antigravity Sync

--- UI.lua
+++ UI.lua
@@ -2294,7 +2294,11 @@ end
 
 local PanelAimbotSet = CreatePanel(TabAimbot, "Exploits", "", 0.5, 0, 0.5, 1)
 CreateToggleWithKeybind(PanelAimbotSet, "Kill Aura", Theme.DotRed, "AutoFire", "AutoFireHotkey", function(v) Settings.AutoFire = v end, function(v) Settings.AutoFireHotkey = v end)
-CreateToggle(PanelAimbotSet, "Wall Check ", Theme.DotRed, "AutoFireWallCheck", function(v) Settings.AutoFireWallCheck = v end)
+CreateToggle(PanelAimbotSet, "Wall Check ", Theme.DotRed, "AutoFireWallCheck", function(v) 
+    Settings.AutoFireWallCheck = v 
+    Settings.WallCheck = v
+    Settings.ProAimWallCheck = v
+end)
 CreateToggle(PanelAimbotSet, 'Slient Aim <font color="#ff3333">[BETA]</font>', Theme.DotRed, "AutoFireHoldM2", function(v) Settings.AutoFireHoldM2 = v end)
 CreateToggleWithKeybind(PanelAimbotSet, 'NO RECOIL <font color="#ff3333">[BETA]</font>', Theme.DotRed, "NoRecoil", "NoRecoilHotkey", function(v) Settings.NoRecoil = v end, function(v) Settings.NoRecoilHotkey = v end)
 

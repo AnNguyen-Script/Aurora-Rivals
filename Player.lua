85aba63db85212542c8d2637010454b4e4cfb6c7
AnNguyen-Script <nguyenminhan952011@gmail.com>
2026-09-23 12:14:55 +0700

    Update Player.lua via Antigravity Sync

--- Player.lua
+++ Player.lua
@@ -231,18 +231,7 @@ return function(Shared, Targeting)
             forEachEnemy(function(char, source)
                 local hum = char:FindFirstChildOfClass("Humanoid")
                 if hum and hum.Health > 0 then
-                    if Settings.AimSafe then
-                        expandPart(char:FindFirstChild("Head"))
-                        expandPart(char:FindFirstChild("HitboxHead"))
-                        expandPart(char:FindFirstChild("PhysicalHitboxHead"))
-                        expandPart(char:FindFirstChild("HitboxHeadSmall"))
-                        expandPart(char:FindFirstChild("HumanoidRootPart"))
-                        expandPart(char:FindFirstChild("UpperTorso"))
-                        expandPart(char:FindFirstChild("Torso"))
-                        expandPart(char:FindFirstChild("HitboxBody"))
-                        expandPart(char:FindFirstChild("PhysicalHitbox"))
-                        expandPart(char:FindFirstChild("HitboxBodySmall"))
-                    elseif isHead then
+                    if isHead then
                         expandPart(char:FindFirstChild("Head"))
                         expandPart(char:FindFirstChild("HitboxHead"))
                         expandPart(char:FindFirstChild("PhysicalHitboxHead"))
